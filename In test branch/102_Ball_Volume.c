2505024a3acad3a09a62b925a6ec88dc5ecf3ff6
JiaWun <jiawunliaw@gmail.com>
2023-09-30 01:53:37 +0800

    delete

--- In test branch/102_Ball_Volume.c
+++ In test branch/102_Ball_Volume.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
 int main(){
-<<<<<<< HEAD
   float w, l, r;
 	scanf("%f %f %f", &w, &l, &r);
 	float V = 3.14 * r * r * r * 4 / 3 ;
 	float h = V / w / l;
 	printf("%f %f", V, h);
   return 0;
-=======
-printf("asdfHelloWorlld");
-return 0;
->>>>>>> 07e15c102e1469ed971c524e7b115e395b07da50
 }
